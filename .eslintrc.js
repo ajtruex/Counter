e7e6c6656daa5f9d5b3cb37b4941a06de22a967c
Andrew Truex <andrewtruex@gmail.com>
2018-07-31 21:05:17 -0400

    Initial

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,13 @@ module.exports = {
   },
   'extends': [
     'plugin:vue/essential',
-    '@vue/standard'
+    '@vue/standard',
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'no-tabs': 'off',
+    'eol-last': 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
+/* eslint-disable */
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js Counter App" />
+    <p>{{count}}</p>
+    <button @click="increment">↑</button>
+    <button @click="decrement">↓</button>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import store from './store'
 
 export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+  computed: {
+    count () {
+      return store.state.count
+    }
+  },
+  methods: {
+    increment () {
+      store.commit('increment')
+    },
+    decrement () {
+      store.commit('decrement')
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: "Avenir", Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
